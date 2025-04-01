a730894ac3dcb2810313c62c8b38863db7ee3551
ThyMuoyhak <thymuoyhak.biu@gmail.com>
2025-04-01 18:54:24 +0700

    update webiste v1.1

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -1,5 +1,18 @@
 <x-layout title="Jobs">
-    <x-slot:heading>Jobs list</x-slot:heading>
-    <h1>Welcome to my list jobs</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem dolorem quam similique ipsam veniam possimus quo consequatur, doloribus nemo nihil tenetur ipsum fugiat, eum sequi minus aut, reprehenderit quasi porro? In nisi recusandae consequuntur nam. Unde corporis laborum architecto ipsa, temporibus laudantium est, ipsam suscipit expedita voluptas, pariatur quia natus.</p>
+    <x-slot:heading>Jobs List</x-slot:heading>
+
+    <h1 class="text-4xl font-bold text-center my-6">Welcome to My Job List</h1>
+
+    <p class="text-gray-600 text-lg text-center max-w-3xl mx-auto">
+        Browse through our latest job openings and find the perfect position for you!
+    </p>
+
+    <div class="mt-8 space-y-6">
+        @foreach($jobs as $job)
+            <div class="bg-white shadow-lg rounded-lg p-6 border border-gray-200">
+                <h2 class="text-2xl font-semibold text-blue-600">Position: {{ $job['position'] }}</h2>
+                <h3 class="text-xl text-gray-800">Salary: {{ $job['salary'] }}</h3>
+            </div>
+        @endforeach
+    </div>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,20 @@ Route::get('/contact', function () {
 });
 
 Route::get('/jobs', function () {
-    return view('jobs');
+    $jobs = [
+        ['position' => 'Manager', 'salary' => '$1,200'],
+        ['position' => 'Software Engineer', 'salary' => '$2,500'],
+        ['position' => 'Data Analyst', 'salary' => '$2,000'],
+        ['position' => 'Marketing Specialist', 'salary' => '$1,800'],
+        ['position' => 'Sales Executive', 'salary' => '$1,500'],
+        ['position' => 'HR Officer', 'salary' => '$1,400'],
+        ['position' => 'Graphic Designer', 'salary' => '$1,600'],
+        ['position' => 'Customer Support', 'salary' => '$1,200'],
+        ['position' => 'IT Support', 'salary' => '$1,300'],
+        ['position' => 'Accountant', 'salary' => '$1,700'],
+    ];
+
+    return view('jobs', ['jobs' => $jobs]); // Pass $jobs to the view
 });
+
+
