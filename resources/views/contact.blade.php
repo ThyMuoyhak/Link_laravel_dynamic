5466b3a2fb2275a51348652036e583833b908ef5
ThyMuoyhak <thymuoyhak.biu@gmail.com>
2025-04-01 18:17:29 +0700

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,5 @@
+<x-layout title="contactpage">
+    <x-slot:heading>Welcome to my Contact</x-slot:heading>
+    <h1>Welcome to my Contactpage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem dolorem quam similique ipsam veniam possimus quo consequatur, doloribus nemo nihil tenetur ipsum fugiat, eum sequi minus aut, reprehenderit quasi porro? In nisi recusandae consequuntur nam. Unde corporis laborum architecto ipsa, temporibus laudantium est, ipsam suscipit expedita voluptas, pariatur quia natus.</p>
+</x-layout>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+<x-layout title="Homepage">
+    <x-slot:heading>Welcome to my Homepage</x-slot:heading>
+    <h1>Welcome to my Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem dolorem quam similique ipsam veniam possimus quo consequatur, doloribus nemo nihil tenetur ipsum fugiat, eum sequi minus aut, reprehenderit quasi porro? In nisi recusandae consequuntur nam. Unde corporis laborum architecto ipsa, temporibus laudantium est, ipsam suscipit expedita voluptas, pariatur quia natus.</p>
+</x-layout>
